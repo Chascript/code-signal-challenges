632293e4b9758cbc08755d3a663b6a4d4e925518
arcticsurgeon <conanc1993@gmail.com>
2021-02-28 13:09:33 +0000

    challenges 1, 2, 3, 4 and 5 committed

---
+++ 4-adjacentElementsProduct.js
@@ -0,0 +1,13 @@
+/*
+Given an array of integers, find the pair of adjacent elements that has the largest product and return that product.
+
+Example
+
+For inputArray = [3, 6, -2, -5, 7, 3], the output should be
+adjacentElementsProduct(inputArray) = 21.
+
+7 and 3 produce the largest product.
+*/
+function adjacentElementsProduct(inputArray) {
+    return Math.max(...inputArray.slice(0,-1).map((value,index) => value*inputArray[index+1]))
+}
---
+++ 5-shapeArea-interestingPolygon.js
@@ -0,0 +1,20 @@
+/*
+Below we will define an n-interesting polygon. Your task is to find the area of a polygon for a given n.
+
+A 1-interesting polygon is just a square with a side of length 1. An n-interesting polygon is obtained by taking the n - 1-interesting polygon and appending 1-interesting polygons to its rim, side by side. You can see the 1-, 2-, 3- and 4-interesting polygons in the picture below.
+
+Example
+
+For n = 2, the output should be
+shapeArea(n) = 5;
+For n = 3, the output should be
+shapeArea(n) = 13.
+*/
+function shapeArea(n) {
+    let area = 1
+    for(let i = 1; i <= n; i++){
+        let x = i * 4 - 4
+        area+= + x
+    }
+    return area
+}
