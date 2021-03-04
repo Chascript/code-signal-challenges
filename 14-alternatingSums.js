4ae07eaab1ba83c17a78bf7556b3db6b0c25c23d
arcticsurgeon <conanc1993@gmail.com>
2021-03-04 13:10:00 +0000

    added alternatingSums

---
+++ 14-alternatingSums.js
@@ -0,0 +1,28 @@
+/*
+Several people are standing in a row and need to be divided into two teams. 
+The first person goes into team 1, the second goes into team 2, the third goes 
+into team 1 again, the fourth into team 2, and so on.
+
+You are given an array of positive integers - the weights of the people. 
+Return an array of two integers, where the first element is the total weight 
+of team 1, and the second element is the total weight of team 2 after the 
+division is complete.
+
+Example
+
+For a = [50, 60, 60, 45, 70], the output should be
+alternatingSums(a) = [180, 105].
+*/
+
+function alternatingSums(a) {
+    let even = 0
+    let odd = 0
+    for(const val of a.entries()){
+        if(val[0] % 2 === 0){
+            even += + val[1]
+        } else {
+            odd += + val[1]
+        }
+    }
+    return [even,odd]
+}
